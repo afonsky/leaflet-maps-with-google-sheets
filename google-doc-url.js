67ccff31897d1799ebf2c6f4d869010328a001df
afonsky <alexey.boldyrev@gmail.com>
2022-06-08 23:04:07 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YRsV4r4E0CapnRSeXCWVtMtc5a2iS3gH567UDjzBgGo/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyD7FLq9bP71A6qq7fxCZ1J06SHDuJRWYjY';
